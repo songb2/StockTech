b3c927da731ab783f615a266cea5bab8629ded99
brucesong <brucesong2010@hotmail.com>
2018-10-28 10:59:55 +0800

    add material design & video-list component

---
+++ StockTech/ClientApp/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { VideoListComponent } from './video-list/video-list.component';
+
+const routes: Routes = [
+  { path: 'videos', component: VideoListComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
