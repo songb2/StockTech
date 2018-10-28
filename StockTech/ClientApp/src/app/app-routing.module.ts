44568271f0415b5b92bae880301e6c91cd628f7a
brucesong <brucesong2010@hotmail.com>
2018-10-28 16:01:38 +0800

    add home & material design

--- StockTech/ClientApp/src/app/app-routing.module.ts
+++ StockTech/ClientApp/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { VideoListComponent } from './video-list/video-list.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'videos', component: VideoListComponent }
 ];
 
--- StockTech/ClientApp/src/app/app.module.ts
+++ StockTech/ClientApp/src/app/app.module.ts
@@ -1,21 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VideoListComponent } from './video-list/video-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MaterialModuleModule } from './material-module/material-module.module';
+
+import { SidenavResponsiveComponent } from './sidenav-responsive/sidenav-responsive.component';
+import { HomeComponent } from './home/home.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    VideoListComponent
+    VideoListComponent,
+    SidenavResponsiveComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MaterialModuleModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
   ],
+  entryComponents: [SidenavResponsiveComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
